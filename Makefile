4eaac56aac7480bd0162f9db0d991d0c762f88a4
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-05-06 12:55:28 -0700

    Initial commit. End of chapter 2.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

---
+++ Makefile
@@ -0,0 +1,2 @@
+oz: oz.c
+	$(CC) oz.c -o oz -Wall -Wextra -pedantic -std=c99
---
+++ oz.c
@@ -0,0 +1,66 @@
+/*** Includes: ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** Data: ***/
+struct termios orig_termios;
+
+/*** Terminal: ***/
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
+}
+
+void enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        die("tcgetattr");
+    }
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
+}
+
+/*** Init: ***/
+int main() {
+    enableRawMode();
+
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            die("read");
+        }
+
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') {
+            break;
+        }
+    }
+
+    return 0;
+}
