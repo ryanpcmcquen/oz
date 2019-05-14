0ccf26bbb8aa68bb8bb568ce4b2955d9676d16c7
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-05-13 17:41:17 -0700

    Step 53.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- oz.c
+++ oz.c
@@ -17,7 +17,11 @@ enum editorKey {
                 ARROW_LEFT = 1000,
                 ARROW_RIGHT,
                 ARROW_UP,
-                ARROW_DOWN
+                ARROW_DOWN,
+                HOME_KEY,
+                END_KEY,
+                PAGE_UP,
+                PAGE_DOWN
 };
 /*** Data: ***/
 struct editorConfig {
@@ -82,17 +86,51 @@ int editorReadKey() {
         }
 
         if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO,  &seq[2], 1) != 1) {
+                    return '\x1b';
+                }
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                    case '1':
+                        return HOME_KEY;
+                    case '4':
+                        return END_KEY;
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    case '7':
+                        return HOME_KEY;
+                    case '8':
+                        return END_KEY;
+                    }
+                }
+            } else {
+                switch (seq[1]) {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
+                }
+            }
+        } else if (seq[0] == 'O') {
             switch (seq[1]) {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
+            case 'H':
+                return HOME_KEY;
+            case 'F':
+                return END_KEY;
             }
         }
+        
         return '\x1b';
     } else {
         return c;
@@ -223,16 +261,24 @@ void editorRefreshScreen() {
 void editorMoveCursor(int key) {
     switch (key) {
     case ARROW_LEFT:
-        E.cx--;
+        if (E.cx != 0)  {
+            E.cx--;
+        }
         break;
     case ARROW_RIGHT:
-        E.cx++;
+        if (E.cx != E.screencols - 1) {
+            E.cx++;
+        }
         break;
     case ARROW_UP:
-        E.cy--;
+        if (E.cy != 0) {
+            E.cy--;
+        }
         break;
     case ARROW_DOWN:
-        E.cy++;
+        if (E.cy != E.screenrows - 1) {
+            E.cy++;
+        }
         break;
     }
 }
@@ -245,6 +291,24 @@ void editorProcessKeypress() {
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
+        break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+
+    case END_KEY:
+        E.cx = E.screencols - 1;
+        break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+        {
+            int times = E.screenrows;
+            while (times--) {
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+        }
         break;
     case ARROW_UP:
     case ARROW_DOWN:
