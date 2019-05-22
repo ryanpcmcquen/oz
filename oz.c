b32d0417084fcc0cf5c3e3ec3b39c22256ca5d57
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-05-22 21:23:48 +0000

    Step 79.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- oz.c
+++ oz.c
@@ -351,14 +351,24 @@ void editorRefreshScreen()
 /*** Input: ***/
 void editorMoveCursor(int key)
 {
+    erow* row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
     case ARROW_LEFT:
         if (E.cx != 0) {
             E.cx--;
+        } else if (E.cy > 0) {
+            E.cy--;
+            E.cx = E.row[E.cy].size;
         }
         break;
     case ARROW_RIGHT:
-        E.cx++;
+        if (row && E.cx < row->size) {
+            E.cx++;
+        } else if (row && E.cx == row->size) {
+            E.cy++;
+            E.cx = 0;
+        }
         break;
     case ARROW_UP:
         if (E.cy != 0) {
@@ -370,6 +380,12 @@ void editorMoveCursor(int key)
             E.cy++;
         }
         break;
+    }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
     }
 }
 
