ddf80f61c91f8942a7929aad738dd575d6b5d1db
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-05-30 12:31:51 -0700

    Step 103. It is an editor now!
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- oz.c
+++ oz.c
@@ -271,6 +271,34 @@ void editorAppendRow(char* s, size_t len)
     E.numrows++;
 }
 
+void editorRowInsertChar(erow* row, int at, int c)
+{
+    if (at < 0 || at > row->size) {
+        at = row->size;
+    }
+    row->chars = realloc(
+        row->chars,
+        row->size + 2);
+
+    memmove(
+        &row->chars[at + 1],
+        &row->chars[at],
+        row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/*** Editor operations: ***/
+void editorInsertChar(int c)
+{
+    if (E.cy == E.numrows) {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 /*** File i/o: ***/
 void editorOpen(char* filename)
 {
@@ -555,6 +583,10 @@ void editorProcessKeypress()
     case ARROW_LEFT:
     case ARROW_RIGHT:
         editorMoveCursor(c);
+        break;
+
+    default:
+        editorInsertChar(c);
         break;
     }
 }
